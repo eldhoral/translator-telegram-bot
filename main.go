d7d5b25d5a23533e5ef7a57f833bb1f13c06d737
eldhoral123 <eldho@samudrainovasi.co.id>
2023-09-21 01:24:36 +0700

    remove built in health check

--- main.go
+++ main.go
@@ -4,20 +4,18 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/go-redis/redis/v8"
-	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"github.com/spf13/cast"
 )
 
 func loadConfig() {
-	_ = godotenv.Load(".env")
-	// if err != nil {
-	// 	log.Panic(err)
-	// }
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("Error in load env file: %v\n ", err)
+	}
 }
 
 func initRedisClient() (client *redis.Client, err error) {
@@ -39,18 +37,5 @@ func initRedisClient() (client *redis.Client, err error) {
 
 func main() {
 	redisClient, _ := initRedisClient()
-	// Init router
-	r := mux.NewRouter()
-	r.HandleFunc("/health", healthCheck).Methods("GET")
-	// start app
-	startApp(r)
 	TelegramBot(redisClient)
-}
-
-func healthCheck(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Server is up :)")
-}
-
-func startApp(r *mux.Router) {
-	http.ListenAndServe(":8085", r)
 }
